e83632da5a32276da7ab8d09287edaa8e1bcc204
Rostyslav Sova <sova.rostislav@gmail.com>
2018-03-06 16:05:56 +0000

    some new styles and add js

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        let target = this.hash;
+        let $target = $(target);
+        let scroll;
+        if($(window).scrollTop()==0){
+            scroll =  ($target.offset().top) - 10
+        }else{
+            scroll =  ($target.offset().top) - 10
+        }
+        $('html, body').stop().animate({
+            'scrollTop': scroll
+        }, 500, 'swing', function () {
+            //window.location.hash = target;
+        });
+    });
+});
+
+$(window).on('scroll', function () {
+   var sections = $('section')
+    , nav = $('nav')
+    , nav_height = nav.outerHeight()
+    , cur_pos = $(this).scrollTop();
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+ 
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+    
+
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
